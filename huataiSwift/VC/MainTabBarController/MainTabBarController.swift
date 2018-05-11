f76f6f8b1bebe216f79909dadfd2c55450997276
黃柏恩 <f26681605@gmail.com>
2018-05-11 17:33:31 +0800

    測試 測試

--- huataiSwift/VC/MainTabBarController/MainTabBarController.swift
+++ huataiSwift/VC/MainTabBarController/MainTabBarController.swift
@@ -53,6 +53,11 @@ class MainTabBarController: UITabBarController {
   
   
   
+  
+  
+  
+  
+  
   func imageWithColor(color: UIColor, size: CGSize) -> UIImage {
     let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
     UIGraphicsBeginImageContextWithOptions(size, false, 0)
