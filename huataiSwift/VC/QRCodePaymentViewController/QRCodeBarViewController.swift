0e3556882ad325638dcd9e64cbba53dee94cef03
sunnyleeyun <sunnylee945@hotmail.com>
2018-05-14 14:19:49 +0800

    Add QRCodeBar imageview

--- huataiSwift/VC/QRCodePaymentViewController/QRCodeBarViewController.swift
+++ huataiSwift/VC/QRCodePaymentViewController/QRCodeBarViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class QRCodeBarViewController: UIViewController {
   
+  @IBOutlet weak var qrcodeImageView: UIImageView!
   override func viewDidLoad() {
     super.viewDidLoad()
     
